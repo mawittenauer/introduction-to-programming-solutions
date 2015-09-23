e7f020dc5752c293016d135f417493645c5ea8ca
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2015-09-23 18:13:52 -0400

    Fix Spacing Issues in Solve The Basics Exercises

--- the_basics/exercise_three.rb
+++ the_basics/exercise_three.rb
@@ -1,7 +1,7 @@
 movie_years = { "The Godfather" => 1972, 
-					 "The Godfather Part Two" => 1974, 
-					 "The Godfather Part Three" => 1990, 
-					 "Goodfellas" => 1990 }
+                "The Godfather Part Two" => 1974, 
+                "The Godfather Part Three" => 1990, 
+                "Goodfellas" => 1990 }
 
 puts movie_years["The Godfather"]
 puts movie_years["The Godfather Part Two"]
